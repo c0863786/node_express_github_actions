0724289f70b82a158b4887be209b402abfe72d94
c0863786 <145859394+c0863786@users.noreply.github.com>
2023-09-30 13:52:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
 
+app.get('/name', function (req, res) {
+    res.send("<h1>Meet Bhanderi c0863786</h1>")
+})
+
+
 //http://localhost:3000/profile
 app.post('/profile', (req, res) => {
   console.log(req.body)
